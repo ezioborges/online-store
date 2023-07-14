d98e1ed4c3d10c1f0a0c1617a795d8b7116c527f
Ezio Borges <eziopborges@gmail.com>
2023-07-14 13:11:01 -0300

    lista categoria de produtos da API

---
+++ src/components/Home/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as api from '../../services/api';
+import './index.css';
+
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      allCategories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getAllCategories();
+  }
+
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/shopping-cart');
+  };
+
+  getAllCategories = async () => {
+    const apiResponse = await api.getCategories().then((categories) => categories);
+    this.setState({ allCategories: apiResponse });
+  };
+
+  render() {
+    const emptyMessage = 'Digite algum termo de pesquisa ou escolha uma categoria.';
+    const { allCategories } = this.state;
+    return (
+      <div>
+        <div className="search">
+          <input className="search-input" />
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="shopping-cart-button"
+            className="search-btn"
+          >
+            pesquisar
+          </button>
+        </div>
+        <div className="empty">
+          <h3 data-testid="home-initial-message">{ emptyMessage }</h3>
+        </div>
+        <div className="line" />
+        <ul>
+          {
+            allCategories.map((categories, index) => (
+              <li key={ index }>
+                <button
+                  type="button"
+                  data-testid="category"
+                >
+                  { categories.name }
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
