882b0c3a95d7440f57dc34ebfbface3f7e6934fc
Ezio Borges <eziopborges@gmail.com>
2023-07-25 19:58:43 -0300

    ajustes no component ProductDetailsCard

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Route } from 'react-router-dom/cjs/react-router-dom';
 import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Main from './components/Main';
 import ShoppingCart from './components/ShoppingCart';
-import ProductDetails from './components/ProductDetails';
+// import ProductDetails from './components/ProductDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductDetailCard from './components/ProductDetailCard';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Route
           exact
           path="/product-details/:id"
-          render={ (props) => <ProductDetails { ...props } /> }
+          render={ (props) => <ProductDetailCard { ...props } /> }
         />
       </Switch>
     </div>
---
+++ src/components/ProductDetailCard.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable react/jsx-max-depth */
+import { Component } from 'react';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import * as api from '../services/api';
+
+export default class ProductDetailCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productDetails: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getProductDetail();
+  }
+
+  getProductDetail = async () => {
+    const { match: { params: { id } } } = this.props;
+    const product = await api.getProductById(id);
+    this.setState({ productDetails: product });
+  };
+
+  shoppingCart = () => {
+    const { history } = this.props;
+    history.push('/shopping-cart');
+  };
+
+  home = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  render() {
+    const { productDetails } = this.state;
+    const { title, thumbnail, price } = productDetails;
+
+    const formarter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2,
+    });
+
+    const priceFormated = formarter.format(price);
+
+    return (
+      <Container>
+        <Row className="d-flex justify-content-center m-5">
+          <Card style={ { width: '30vw' } }>
+            <Card.Img variant="top" src={ thumbnail } alt={ title } />
+            <Card.Body>
+              <Card.Title>{ title }</Card.Title>
+              <Card.Footer className="d-flex justify-content-center">
+                {priceFormated}
+              </Card.Footer>
+              <Row className="d-flex flex-row justify-content-center mt-3">
+                <Col className="d-flex justify-content-center">
+                  <Button
+                    variant="primary"
+                    onClick={ this.home }
+                    // className="py-4"
+                    style={ { height: '10vh', width: '10vw' } }
+                  >
+                    voltar para tela inicial
+                  </Button>
+                </Col>
+                <Col className="d-flex justify-content-center">
+                  <Button
+                    // className="py-4"
+                    style={ { height: '10vh', width: '10vw' } }
+                    onClick={ this.shoppingCart }
+                    data-testid="shopping-cart-button"
+                  >
+                    Adicionar ao carrinho de compras
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+ProductDetailCard.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
